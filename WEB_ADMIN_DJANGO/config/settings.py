169509959889dbf07805b4f9e5727d7cb756951d
Dilshod Xasanov <dilshod@example.com>
2026-07-28 16:38:31 +0500

    Add STATIC_ROOT

--- WEB_ADMIN_DJANGO/config/settings.py
+++ WEB_ADMIN_DJANGO/config/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.parent / 'WEB_ADMIN' / 'static_root'
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'WEB_ADMIN' / 'static',
 ]
